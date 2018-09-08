aa0224d8a4e5a9267c7ecfa9a56ba8b4f2d69742
rakesh <rakeshkumarjha@live.in>
2018-09-09 01:24:08 +0530

    changed base url

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 var env = process.env.NODE_ENV || 'development'
 var config = {
     development: {
-        baseUrl:'http://127.0.0.1',
+        baseUrl:'http://13.127.62.240',
         ingest:false,
         port: 5003,
         baseURI: '',
