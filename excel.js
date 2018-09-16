9abda53d28b885e4c5d777282065f6e555f75671
rakesh <rakeshkumarjha@live.in>
2018-09-16 06:53:41 +0530

    new code

--- excel.js
+++ excel.js
@@ -3,7 +3,10 @@ const client = require('./lib/elasticSearch')
 const elasticQuery=require('./lib/query')
 var Excel = require('exceljs');
 
-function processFileAndWrite(col,row_start,phrase_column,result_column,worksheet,workbook,length,filename,index,exactMatch,callback) {
+function processFileAndWrite(col,row_start,phrase_column,result_column,worksheet,workbook,length,filename,index,exMatch) {
+    return new Promise(function (resolve, reject) {
+
+
     let defaultStart=row_start || null
     let query;
     let writeCount=0
@@ -14,6 +17,12 @@ function processFileAndWrite(col,row_start,phrase_column,result_column,worksheet
         worksheet.getRow(1).getCell(phraseColumn).value = "Search phrase"
         worksheet.getRow(1).getCell(resultColumn).value = "Search results"
     }
+    let exactMatch=true
+    if(exMatch=="true") {
+         exactMatch=true
+    }else{
+        exactMatch=false
+    }
 
     col.eachCell(function (cell, rowNumber) {
         if(rowNumber>=rowStart) {
@@ -23,12 +32,13 @@ function processFileAndWrite(col,row_start,phrase_column,result_column,worksheet
                 query = elasticQuery.prepareQuery({text: searchString},exactMatch)
             }else if('customer'){
                 query=elasticQuery.prepareCustomerQuery({text:searchString},exactMatch)
+
             }
             var dataBaseIndex=index
-            MassSearch(rowNumber,dataBaseIndex,searchString, query)
+            MassSearch(rowNumber,dataBaseIndex,searchString, query,exactMatch)
                 .then(function (cb) {
                     if(cb.totalNumber>0){
-                        worksheet.getRow(cb.rowNumber).getCell(resultColumn).value= { text: 'Search', hyperlink: config.baseUrl+'/?search-text='+searchString+'&index='+index}
+                        worksheet.getRow(cb.rowNumber).getCell(resultColumn).value= { text: 'Search', hyperlink: config.baseUrl+'/?search-text='+searchString+'&index='+index+'&exactMatch='+exactMatch}
                         worksheet.getRow(cb.rowNumber).getCell(resultColumn).font = {
                             underline: true,
 
@@ -40,18 +50,22 @@ function processFileAndWrite(col,row_start,phrase_column,result_column,worksheet
                     }
                     workbook.xlsx.writeFile(__dirname + filename).then(function() {
                         writeCount=writeCount+1
-                        if(writeCount>=length-rowStart){
-                            callback('done')
+                        if(writeCount>=length-parseInt(rowStart)){
+                            resolve('done')
                         }
 
                     }).catch(function (error) {
+                        console.log(error)
                         console.log('processFileAndWrite error')
                         console.log(error)
+                        reject('something went wrong')
                     })
                 }).catch(function (error) {
+                    reject('something went wrong')
                 console.log(error)
             })
         }
+    })
     })
 }
 function uploadFileAndWrtite(filePath,row_start,phrase_column,result_column,index,exactMatch,callback) {
@@ -61,23 +75,22 @@ function uploadFileAndWrtite(filePath,row_start,phrase_column,result_column,inde
     var workbook = new Excel.Workbook();
     var filename = '/'+filePath;
     // Reading excel file
-    workbook.xlsx.readFile(__dirname + filename).then(function() {
+    workbook.xlsx.readFile(__dirname + filename).then(async function() {
         // Get the 2nd column ('input')
         var worksheet = workbook.getWorksheet(1);
         // Get the B column
         let phraseColumn=phrase_column || "A"
         var col = worksheet.getColumn(phraseColumn)
-        processFileAndWrite(col,row_start,phrase_column,result_column,worksheet,workbook,worksheet.rowCount, filename,index,exactMatch,function (res) {
+        try {
 
+            let res= await  processFileAndWrite(col,row_start,phrase_column,result_column,worksheet,workbook,worksheet.actualRowCount, filename,index,exactMatch)
             callback(res)
-        })
-
-    }).catch(function (error) {
-        console.log('uploadFileAndWrtite error')
-        console.log(error)
+        }catch (e) {
+            callback(e)
+        }
     })
 }
-function MassSearch(rowNum,index,searchString,query) {
+function MassSearch(rowNum,index,searchString,query,exactMatch) {
     return new Promise(function (resolve,reject) {
         const type = config.elasticSearch.profileType
         const source = []
@@ -102,9 +115,7 @@ function MassSearch(rowNum,index,searchString,query) {
         esQuery.body = {
             query: query
         }
-        if (source) {
-            esQuery.body._source = source
-        }
+        esQuery.body._source=['_id']
         if (sort) {
             esQuery.body.sort = sort
         }
@@ -118,26 +129,26 @@ function MassSearch(rowNum,index,searchString,query) {
                         // //     return results._source
                         // })
                         let total = esDocs.hits.total
+
                         // if (result.length < 1) {
                         //     total=0
                         // }
-                        console.log('totalNumber '+total)
-                        resolve({totalNumber:total,searchString:searchString,rowNumber:rowNum})
+                       // console.log('totalNumber '+total)
+                        resolve({totalNumber:total,exactMatch:exactMatch,searchString:searchString,rowNumber:rowNum})
 
                     } else {
-                        resolve({totalNumber:0,searchString:searchString,rowNumber:rowNum})
+                        resolve({totalNumber:0,exactMatch:exactMatch,searchString:searchString,rowNumber:rowNum})
 
                     }
                 }
             ).catch(function (error) {
-            console.log(error)
-            console.log('MassSearch client search error')
-            resolve({totalNumber:0,searchString:searchString,rowNumber:rowNum})
+            resolve({totalNumber:0,exactMatch:exactMatch,searchString:searchString,rowNumber:rowNum})
 
 
         }).catch(function (error) {
             console.log('MassSearch error')
             console.log(error)
+            reject('something went wrong')
         })
     })
 
